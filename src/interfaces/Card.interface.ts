d5e023b74ef0a20367a4a0e248dc8d943cd06bd1
RebeccaBrasileiro <rebeccabrasileiro28@gmail.com>
2022-05-11 10:48:23 -0300

    aulafinalweb

---
+++ src/interfaces/Card.interface.ts
@@ -0,0 +1,13 @@
+ export interface CardProps {
+    data: {
+      id: number,
+      nome: string,
+      data: Date,
+      titulo: string,
+      mensagem: string,
+      topico: {
+        id: number,
+        item: string
+      }[]
+    }
+  }
---
+++ src/styles/colors.ts
@@ -0,0 +1,15 @@
+export default {
+    black: '#000000',
+    white: '#ffffff',
+    green: '#455A64',
+    greenLight: '#CFD8DC',
+    purple: '#7E57C2',
+    purpleLight: '#B39DDB',
+    orange: '#FFB74D',
+    orangeLight: '#FFE9CA',
+    red: '#C50000',
+    pinka: '#f7c0f9 ',
+    salmao: '#ffcaca' ,
+    redc:' #ff4d4d ',
+  }
+  
